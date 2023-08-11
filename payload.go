886239cb28fe943910c96810d3a455c802706cb5
randal.goss <randal.s.goss@usace.army.mil>
2023-08-11 11:21:50 -0400

    Added PayloadAttributes type and methods to get attributes as int, float, or string tyfpes

--- payload.go
+++ payload.go
@@ -1,5 +1,13 @@
 package cc
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+
+	"github.com/spf13/cast"
+)
+
 type Action struct {
 	Name        string         `json:"name"`
 	Type        string         `json:"type,omitempty"`
@@ -8,9 +16,57 @@ type Action struct {
 }
 
 type Payload struct {
-	Attributes map[string]interface{} `json:"attributes,omitempty"`
-	Stores     []DataStore            `json:"stores"`
-	Inputs     []DataSource           `json:"inputs"`
-	Outputs    []DataSource           `json:"outputs"`
-	Actions    []Action               `json:"actions"`
+	Attributes PayloadAttributes `json:"attributes,omitempty"`
+	Stores     []DataStore       `json:"stores"`
+	Inputs     []DataSource      `json:"inputs"`
+	Outputs    []DataSource      `json:"outputs"`
+	Actions    []Action          `json:"actions"`
+}
+
+type PayloadAttributes map[string]interface{}
+
+func (p PayloadAttributes) GetIntAttr(name string) (int, error) {
+	i, err := GetAttribute[int64](p, name)
+	return int(i), err
+}
+
+func (p PayloadAttributes) GetInt64Attr(name string) (int64, error) {
+	return GetAttribute[int64](p, name)
+}
+
+func (p PayloadAttributes) GetFloatAttr(name string) (float64, error) {
+	return GetAttribute[float64](p, name)
+}
+
+func (p PayloadAttributes) GetStringAttr(name string) (string, error) {
+	return GetAttribute[string](p, name)
+}
+
+type PayloadAttributeTypes interface {
+	int64 | int32 | float64 | string
+}
+
+func GetAttribute[T PayloadAttributeTypes](pa PayloadAttributes, name string) (T, error) {
+	var t T
+	if attr, ok := pa[name]; ok {
+		tv := reflect.ValueOf(t)
+		tk := tv.Kind()
+		switch tk {
+		case reflect.Int32, reflect.Int64:
+			i, err := cast.ToIntE(attr)
+			tv.Set(reflect.ValueOf(i))
+			return t, err
+		case reflect.String:
+			s, err := cast.ToStringE(attr)
+			tv.Set(reflect.ValueOf(s))
+			return t, err
+		case reflect.Float64:
+			f, err := cast.ToFloat64E(attr)
+			tv.Set(reflect.ValueOf(f))
+			return t, err
+		default:
+			return t, errors.New("Unsupported type for cast")
+		}
+	}
+	return t, errors.New(fmt.Sprintf("Attribute %s is not in the payload\n", name))
 }
